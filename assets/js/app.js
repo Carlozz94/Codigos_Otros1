101c72cafab623129e5562bc17460ecf5559a6d3
CarlosDev <cazc.zarate94@gmail.com>
2023-03-22 11:58:41 -0600

    Ejercicio terminado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,27 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const username ="stolinski";
+const $n = document.getElementById('name'); // Cambiado 'name' por '#name'
+const $b = document.getElementById('blog');
+const $l = document.getElementById('location');
 
-function displayUser(username) {
+async function displayUser(username) { // Agregado 'async'
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    const data = await response.json(); // Agregado 'await' y convertido a JSON
+    return data;   
 }
+
+displayUser(username).then(data =>{
+  
+  $n.textContent = data.name; // Cambio '${data.name}' por 'data.name'
+  $b.textContent = data.blog;
+  $l.textContent = data.location;
+  console.log(data);
+}).catch(handleError);
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`; // Agregado '$' para interpolar la variable err
 }
-
-displayUser('stolinski').catch(handleError);
